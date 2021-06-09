c5478e5600ead1e711a340656f6654670728913d
EC2 Default User <boc.jrock@gmail.com>
2021-06-09 01:28:08 +0000

    [Add],date.get&date.viw&blog.roop

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
@@ -21,7 +22,7 @@ class BlogsController < ApplicationController
 
   private
   def blog_params
-    params.require(:blog).permit(:title, :categry, :body)
+    params.require(:blog).permit(:title, :category, :body)
 
   end
 end
